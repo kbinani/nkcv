71660a319673b24ecdf5a9ac65d687a13f906c9c
kbinani <kbinani.bt@gmail.com>
2018-09-14 23:35:12 +0900

    fix: ファイルパスを間違えていた

--- script/install_cert.sh
+++ script/install_cert.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-openssl aes-256-cbc -k $DECORD_CERTS -in scripts/certs/cert.cer.enc -d -a -out scripts/certs/cert.cer
-openssl aes-256-cbc -k $DECORD_CERTS -in scripts/certs/cert.p12.enc -d -a -out scripts/certs/cert.p12
+openssl aes-256-cbc -k $DECORD_CERTS -in script/certs/cert.cer.enc -d -a -out script/certs/cert.cer
+openssl aes-256-cbc -k $DECORD_CERTS -in script/certs/cert.p12.enc -d -a -out script/certs/cert.p12
 
 security create-keychain -p travis nkcv-build.keychain
 security default-keychain -s nkcv-build.keychain
@@ -9,5 +9,5 @@ security unlock-keychain -p travis nkcv-build.keychain
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/nkcv-build.keychain
 
 security import ./script/certs/AppleWWDRCA.cer -k ~/Library/Keychains/nkcv-build.keychain -T /usr/bin/codesign
-security import ./scripts/certs/cert.cer -k ~/Library/Keychains/nkcv-build.keychain -T /usr/bin/codesign
-security import ./scripts/certs/cert.p12 -k ~/Library/Keychains/nkcv-build.keychain -P $CERTS_PASS -T /usr/bin/codesign
+security import ./script/certs/cert.cer -k ~/Library/Keychains/nkcv-build.keychain -T /usr/bin/codesign
+security import ./script/certs/cert.p12 -k ~/Library/Keychains/nkcv-build.keychain -P $CERTS_PASS -T /usr/bin/codesign
