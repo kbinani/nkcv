783cfd3b40096777cbd6413ce8682fd67adb5441
kbinani <kbinani.bt@gmail.com>
2018-11-27 01:14:10 +0900

    cleanup: KDock のコードを整理

--- src/KDock.js
+++ src/KDock.js
@@ -2,41 +2,44 @@
 
 const _ = require('lodash');
 
-function KDock(data, master) {
-  this._data = data;
-  this._master = master;
+class KDock {
+  constructor(data, master) {
+    this._data = data;
+    this._master = master;
+  }
+
+  ships() {
+    return this._data.map((data) => {
+      return new KDockShip(data, this._master);
+    });
+  }
+
+  complete(index) {
+    _.set(this._data, [index, 'api_complete_time'], 0);
+    _.set(this._data, [index, 'api_complete_time_str'], '0');
+    _.set(this._data, [index, 'api_state'], 3);
+  }
 }
 
-function KDockShip(data, master) {
-  this._data = data;
-  const mst_id = _.get(data, ['api_created_ship_id'], -1);
-  this._master = master.ship(mst_id);
+class KDockShip {
+  constructor(data, master) {
+    this._data = data;
+    const mst_id = _.get(data, ['api_created_ship_id'], -1);
+    this._master = master.ship(mst_id);
+  }
+
+  name() {
+    return _.get(this._master, ['api_name'], '' + _.get(this._data, ['api_created_ship_id'], 0));
+  }
+
+  complete_time() {
+    const ms = _.get(this._data, ['api_complete_time'], 0);
+    return new Date(ms);
+  }
+
+  state() {
+    return _.get(this._data, ['api_state'], -1);
+  }
 }
-
-KDockShip.prototype.name = function() {
-  return _.get(this._master, ['api_name'], '' + _.get(this._data, ['api_created_ship_id'], 0));
-};
-
-KDockShip.prototype.complete_time = function() {
-  const ms = _.get(this._data, ['api_complete_time'], 0);
-  return new Date(ms);
-};
-
-KDockShip.prototype.state = function() {
-  return _.get(this._data, ['api_state'], -1);
-};
-
-KDock.prototype.ships = function() {
-  const self = this;
-  return this._data.map((data) => {
-    return new KDockShip(data, self._master);
-  });
-};
-
-KDock.prototype.complete = function(index) {
-  _.set(this._data, [index, 'api_complete_time'], 0);
-  _.set(this._data, [index, 'api_complete_time_str'], '0');
-  _.set(this._data, [index, 'api_state'], 3);
-};
 
 module.exports = KDock;
