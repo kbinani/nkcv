204cb667afb598510ec7baea677539886eef67f8
kbinani <kbinani.bt@gmail.com>
2018-11-28 00:27:42 +0900

    cleanup: CreatedSlotitem.js を整理

--- src/CreatedSlotitem.js
+++ src/CreatedSlotitem.js
@@ -2,13 +2,15 @@
 
 const _ = require('lodash');
 
-function CreatedSlotitem(slotitem_master, slotitem) {
-  this._slotitem_master = slotitem_master;
-  this.slotitem = slotitem;
-}
+class CreatedSlotitem {
+  constructor(slotitem_master, slotitem) {
+    this._slotitem_master = slotitem_master;
+    this.slotitem = slotitem;
+  }
 
-CreatedSlotitem.prototype.name = function() {
-  return _.get(this._slotitem_master, ['api_name'], '');
-};
+  name() {
+    return _.get(this._slotitem_master, ['api_name'], '');
+  }
+}
 
 module.exports = CreatedSlotitem;
