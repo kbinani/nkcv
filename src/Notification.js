b070169fd931784ae60df3bc0ac9d5713866c2f6
kbinani <kbinani.bt@gmail.com>
2018-11-29 00:09:23 +0900

    cleanup: Notification.js を整理

--- src/Notification.js
+++ src/Notification.js
@@ -2,27 +2,26 @@
 
 const os = require('os');
 
-function Notification() {
-}
-
-Notification.show = function(message) {
-  if (os.platform() == 'win32') {
-    const notifier = require('node-notifier');
-    notifier.notify({
-      'title': 'nkcv',
-      'message': message,
-      'sound': true,
-    });
-  } else {
-    const {ipcMain, ipcRenderer} = require('electron');
-    if (ipcMain) {
-      const NativeNotification = require('electron').Notification;
-      const n = new NativeNotification({'title': 'nkcv', 'body': message});
-      n.show();
+class Notification {
+  static show(message) {
+    if (os.platform() == 'win32') {
+      const notifier = require('node-notifier');
+      notifier.notify({
+        'title': 'nkcv',
+        'message': message,
+        'sound': true,
+      });
     } else {
-      ipcRenderer.send('app._notification', message);
+      const {ipcMain, ipcRenderer} = require('electron');
+      if (ipcMain) {
+        const NativeNotification = require('electron').Notification;
+        const n = new NativeNotification({'title': 'nkcv', 'body': message});
+        n.show();
+      } else {
+        ipcRenderer.send('app._notification', message);
+      }
     }
   }
-};
+}
 
 module.exports = Notification;
