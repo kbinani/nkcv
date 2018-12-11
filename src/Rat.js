f5da9189cc0b14def6fec18e87969645deca02a2
kbinani <kbinani.bt@gmail.com>
2018-12-12 00:51:36 +0900

    cleanup: Rat.js を整理

--- src/Rat.js
+++ src/Rat.js
@@ -1,42 +1,42 @@
 'use strict;'
 
-function Rat(num, denom) {
-  this._num = num;
-  this._denom = denom;
-}
-
-var _ = Rat;
-
-_.prototype.numerator = function() {
-  return this._num;
-};
-
-_.prototype.denominator = function() {
-  return this._denom;
-}
-
-_.prototype.value = function() {
-  return this._num / this._denom;
-}
-
-_.prototype.toString = function() {
-  return this._num + "/" + this._denom;
-};
-
-Rat.fromString = function(str) {
-  const item = str.split('/').map((it) => parseInt(it, 10));
-  if (item.length != 2) {
-    return null;
+class Rat {
+  constructor(num, denom) {
+    this._num = num;
+    this._denom = denom;
   }
-  const num = item[0];
-  const denom = item[1];
-  if (isNaN(num) || isNaN(denom)) {
-    return null;
+
+  numerator() {
+    return this._num;
   }
-  if (denom <= 0) {
-    return null;
+
+  denominator() {
+    return this._denom;
   }
-  return new Rat(num ,denom);
-};
+
+  value() {
+    return this._num / this._denom;
+  }
+
+  toString() {
+    return this._num + "/" + this._denom;
+  }
+
+  static fromString(str) {
+    const item = str.split('/').map((it) => parseInt(it, 10));
+    if (item.length != 2) {
+      return null;
+    }
+    const num = item[0];
+    const denom = item[1];
+    if (isNaN(num) || isNaN(denom)) {
+      return null;
+    }
+    if (denom <= 0) {
+      return null;
+    }
+    return new Rat(num ,denom);
+  }
+}
 
 module.exports = Rat;
